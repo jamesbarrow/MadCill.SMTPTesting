92a2709fe74e2dd5a92502d4766f21ccc0bcac78
James Barrow <james.barrow@sitecore.com>
2020-03-30 10:18:32 +1100

    Powershell test

---
+++ Powershell-SendMailMessageTest.ps1
@@ -0,0 +1,27 @@
+﻿$date = Get-Date -Format yyyy-MM-dd
+$time = get-date -Format HH:mm:ss
+
+$username = $null
+$password = $null
+#$username = "domain\user"
+#$password = 'password'
+
+$smtpHost = "localhost"
+$port = 25
+
+$to = "test@localhost"
+$from = "from@localhost"
+
+$subject = "This is a test message - $($date + "|" + $time)"
+$body = "This is a test body"
+
+
+if($username -ne $null) {
+    $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+
+    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $smtpHost -Port $port -Credential $cred
+}
+else{
+    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $smtpHost -Port $port
+}
